114e24f8bcf897adddef13055e604109c73f1f12
Danijel <danijel.klobucar@tvz.hr>
2023-05-30 17:48:28 +0200

    updates

--- Front-End/java_project/src/App.js
+++ Front-End/java_project/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar.tsx';
+import Navbar_ from './components/Navbar/Navbar.tsx';
 import HomePage from './pages/HomePage/HomePage.tsx';
 import { Routes, Route} from "react-router-dom";
 import PlaceToVisit from './pages/PlacesToVisit/PlacesToVisit.tsx';
@@ -7,11 +7,12 @@ import PlacesToEat from './pages/PlacesToEat/PlacesToEat.tsx';
 import PlacesToStay from './pages/PlacesToStay/PlacesToStay.tsx';
 import PublicTransport from './pages/PublicTransport/PublicTransport.tsx';
 import SignUp from './pages/SignUp/SignUp.tsx';
+import SignIn from './pages/SignIn/SignIn.tsx';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar_ />
       <Routes>
         <Route exact path = "/" element = {<HomePage></HomePage>}></Route>
         <Route exact path = "/placetovisit" element = {<PlaceToVisit></PlaceToVisit>}></Route>
@@ -19,6 +20,7 @@ function App() {
         <Route exact path = "/placestostay" element = {<PlacesToStay></PlacesToStay>}></Route>
         <Route exact path = "/publictransport" element = {<PublicTransport></PublicTransport>}></Route>
         <Route exact path = "/signup" element = {<SignUp></SignUp>}></Route>
+        <Route exact path = "/signin" element = {<SignIn></SignIn>}></Route>
       </Routes>
       
     </div>
